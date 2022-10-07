f9de41f90b3e23924568dcd4ed70afdad285473b
Mark A. Greenslade <asladeofgreen@pm.me>
2022-10-07 15:14:19 +0200

    1. Introduced dedicated algo executor.

--- tests/test_algos.py
+++ tests/test_algos.py
@@ -4,13 +4,18 @@ import pytest
 
 from pyactus import algos
 from pyactus import codecs
+from pyactus.types.enums import ContractType
 from pyactus.types.terms import ContractTermset
 from pyactus.types.terms import CONTRACT_TERMSETS
 
 
 def test_that_a_contract_event_sequence_can_be_calculated(test_contracts: typing.List[dict]):
     for contract_type, fixture in test_contracts:
-        event_sequence = []
+        if contract_type != ContractType.ANN:
+            continue
+
         termset = codecs.decode_from_dict(fixture["terms"], ContractTermset)
-        with pytest.raises(KeyError):
-            algos.execute_step(contract_type, event_sequence, termset, None)
+
+        event_sequence = []
+        algos.execute_step(contract_type, event_sequence, termset, None)
+
